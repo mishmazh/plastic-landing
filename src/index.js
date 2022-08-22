64525d642cb6cc2f56a3f4690599636f8f6a584b
xofyerg <xofyerg1@gmail.com>
2022-08-22 19:40:10 +0300

    swiper, modal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import "bootstrap";
 import "./index.html";
 import "./index.scss";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import Swiper, { Navigation, Pagination } from "swiper";
 
-console.log(2 ** 5);
+new Swiper(".swiper", {
+  modules: [Navigation, Pagination],
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
